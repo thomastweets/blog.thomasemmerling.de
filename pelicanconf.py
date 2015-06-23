7abd589d7e8381d3bc57f4fa3753d9129f33df0a
Thomas Emmerling <git@thomasemmerling.de>
2015-06-23 22:58:15 +0200

    Add social accounts/icons, fontawesome, and a more obvious 'read mode' link for article summaries. Add tagline.

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,6 +5,7 @@ from __future__ import unicode_literals
 AUTHOR = u'Thomas Emmerling'
 SITENAME = u'thomastweets'
 SITEURL = ''
+TAGLINE = 'notes on data analysis, programming, and the rest'
 LOCALE = u'en_US.utf8'
 
 THEME = "themes/svbhack"
@@ -26,6 +27,10 @@ TWITTER_USERNAME = "thomastweets"
 GITHUB_URL = "https://github.com/thomastweets"
 USER_LOGO_URL = "https://www.gravatar.com/avatar/46624cc77f30ff81855f6fdd94fbd6f1?s=140"
 DISQUS_SITENAME = "thomastweets"
+
+SOCIAL = (('fa-twitter-square', 'http://twitter.com/thomastweets'),
+          ('fa-linkedin-square', 'https://www.linkedin.com/in/thomasemmerling'),
+          ('fa-github-square', 'http://github.com/thomastweets'),)
 
 STATIC_PATHS = ['images', 'extra/CNAME']
 EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
